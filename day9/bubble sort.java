0dd65c96dbda86e16c7ee72b321c764d1eb45aff
SACHIN2440 <sachinmukri03@gmail.com>
2024-11-12 13:06:00 +0530

    tjh

--- day9/bubble sort.java
+++ day9/bubble sort.java
@@ -31,8 +31,8 @@ public class BubbleSort {
     public static void main(String[] args) {
         int[] arr = {64, 34, 25, 12, 22, 11, 90};
 
-        System.out.println("Unsorted array:");
-        printArray(arr);
+        
+        
 
         bubbleSort(arr);
 
